2fa041df0fddc1dace70926eef864258c8f28267
Arve Seljebu <arve.seljebu@gmail.com>
2020-01-22 21:41:00 +0100

    actually generate random numbers

--- benches/sqlite.rs
+++ benches/sqlite.rs
@@ -22,7 +22,8 @@ const DB_FILENAME: &str = "storage/bench.sqlite";
 fn sqlite_benchmark(c: &mut Criterion) {
     c.bench_function("get random id", |b| {
         // make sure time(generating random id) <<< time(get random row)
-        b.iter(|| get_random_id())
+        let mut rng = create_rng();
+        b.iter(|| get_random_id(&mut rng))
     });
 
     c.bench_function("prepare statement", |b| {
@@ -30,7 +31,7 @@ fn sqlite_benchmark(c: &mut Criterion) {
         b.iter(|| db.prepare("SELECT value FROM bench WHERE id = ?1"))
     });
 
-    c.bench_function("DEFAULT - unprepared statement", |b| {
+    c.bench_function("DEFAULT - get with unprepared statement", |b| {
         let db = create_filled_db(NUMBER_OF_ROWS);
 
         b.iter(|| {
@@ -45,10 +46,11 @@ fn sqlite_benchmark(c: &mut Criterion) {
     c.bench_function("DEFAULT - get same row", |b| {
         let db = create_filled_db(NUMBER_OF_ROWS);
         let mut get_value = get_value_statement(&db);
+        let mut rng = create_rng();
 
         b.iter(|| {
-            run_query(&mut get_value, || {
-                get_random_id();
+            run_query(&mut get_value, &mut rng, |x| {
+                get_random_id(x);
                 5431
             })
         })
@@ -57,18 +59,20 @@ fn sqlite_benchmark(c: &mut Criterion) {
     c.bench_function("DEFAULT - get random row", |b| {
         let db = create_filled_db(NUMBER_OF_ROWS);
         let mut get_value = get_value_statement(&db);
+        let mut rng = create_rng();
 
-        b.iter(|| run_query(&mut get_value, get_random_id))
+        b.iter(|| run_query(&mut get_value, &mut rng, get_random_id))
     });
 
     c.bench_function("WAL - get same row", |b| {
         let db = create_filled_db(NUMBER_OF_ROWS);
         enable_write_ahead_logging(&db);
         let mut get_value = get_value_statement(&db);
+        let mut rng = create_rng();
 
         b.iter(|| {
-            run_query(&mut get_value, || {
-                get_random_id();
+            run_query(&mut get_value, &mut rng, |x| {
+                get_random_id(x);
                 5431
             })
         })
@@ -78,8 +82,9 @@ fn sqlite_benchmark(c: &mut Criterion) {
         let db = create_filled_db(NUMBER_OF_ROWS);
         enable_write_ahead_logging(&db);
         let mut get_value = get_value_statement(&db);
+        let mut rng = create_rng();
 
-        b.iter(|| run_query(&mut get_value, get_random_id))
+        b.iter(|| run_query(&mut get_value, &mut rng, get_random_id))
     });
 }
 
@@ -133,15 +138,18 @@ fn enable_write_ahead_logging(db: &Connection) {
     assert!("wal" == &result);
 }
 
-fn run_query<F>(statement: &mut Statement, get_id: F) -> u32
+fn run_query<F>(statement: &mut Statement, rng: &mut SmallRng, get_id: F) -> u32
 where
-    F: Fn() -> u32,
+    F: Fn(&mut SmallRng) -> u32,
 {
-    let id = get_id();
+    let id = get_id(rng);
     statement.query_row(&[id], |r| r.get(0)).unwrap()
 }
 
-fn get_random_id() -> u32 {
+fn create_rng() -> SmallRng {
     SmallRng::from_seed([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 2, 3, 4, 5, 6, 7])
-        .gen_range(0, NUMBER_OF_ROWS)
+}
+
+fn get_random_id(rng: &mut SmallRng) -> u32 {
+    rng.gen_range(0, NUMBER_OF_ROWS)
 }
